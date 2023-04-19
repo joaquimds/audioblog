36459bd05c43f4fae9d9e70c454767f0d2ed98bb
Joaquim <joaquimds@gmail.com>
2023-04-19 15:28:34 +0200

    feat: update site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Audioblogs",
+  title: "Audioblog",
   description: "Recordings",
+  openGraph: {
+    title: 'Audioblog',
+    description: 'What are we talking about?',
+    url: 'https://audio.joaqu.im',
+    siteName: 'Audioblog',
+    images: [
+      {
+        url: 'https://audio.joaqu.im/logo.png',
+        width: 192,
+        height: 192,
+      },
+    ],
+    locale: 'en-GB',
+    type: 'website',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
